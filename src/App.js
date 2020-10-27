9059b7f79207f93b13cca072a6c4662bb51de377
Vincent Muriuki <vincentmuriuki42@gmail.com>
2020-10-27 22:31:06 +0300

    Structure the entire application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,119 @@
-import logo from './logo.svg';
+import {
+  Card,
+  CardContent,
+  FormControl,
+  MenuItem,
+  Select,
+} from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import './App.css';
+import InfoBox from './components/InfoBox';
+import Table from './components/Table';
+import Map from './components/Map';
+import { sortData } from './util';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+
+  // https://disease.sh/v3/covid-19/countries
+  // useEffect - runs a piece of code based on a given condition
+
+  // The code here runs once when the component loadsand not again after if the brackets below are left empty
+  // If a avariable is passed, the code will run once when the component loads and again when the variable inside changes
+  // async -> send a request to server and wait for response
+
+  useEffect(() => {
+    fetch(`${process.env.BASE_URL}/v3/covid-19/all`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch(`${process.env.BASE_URL}/v3/covid-19/countries`)
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          const sortedData = sortData(data);
+          setTableData(sortedData);
+
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+    const url =
+      countryCode === 'worldwide'
+        ? `${process.env.BASE_URL}/v3/covid-19/all`
+        : `${process.env.BASE_URL}/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+      });
+  };
+  console.log(countryInfo);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='app__left'>
+        <div className='app__header'>
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className='app__dropdown'>
+            <Select
+              variant='outlined'
+              value={country}
+              onChange={onCountryChange}
+            >
+              <MenuItem value='worldwide'>Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className='app__stats'>
+          <InfoBox
+            title='Coronavirus cases'
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          <InfoBox
+            title='Recovered'
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          <InfoBox
+            title='Deaths'
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
+        </div>
+        <Map />
+      </div>
+
+      <Card className='app__right'>
+        <CardContent>
+          <h3>Live cases by Country</h3>
+          <Table countries={tableData} />
+          <h3>Worldwide new cases</h3>
+        </CardContent>
+
+        {/* Graph */}
+      </Card>
     </div>
   );
 }
---
+++ src/util.js
@@ -0,0 +1,17 @@
+// export const sortData = (data) => {
+//   const sortedData = [...data];
+
+//   sortedData.sort((a, b) => {
+//     if (a.cases > b.cases) {
+//       return -1;
+//     } else {
+//       return 1;
+//     }
+//   });
+//   return sortedData;
+// };
+
+export const sortData = (data) => {
+  const sortedData = [...data];
+  return sortedData.sort((a, b) => a.cases > b.cases? -1 : 1)
+}
